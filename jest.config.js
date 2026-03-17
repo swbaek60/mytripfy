19866d9d6fd23014025683455e438bac9bbbf75a
swbaek60 <swbaek60@gmail.com>
2026-03-17 12:00:12 +0800

    Update

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** Jest 설정 – 에이전트가 `npm test`로 테스트 실행 가능하도록 */
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.[jt]s', '**/*.test.[jt]s'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!**/node_modules/**'],
+};
